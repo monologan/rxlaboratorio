018dfdd6c1d780f3cbd83430549a819714cf4dd1
Gerardo Valdes <monologan@gmail.com>
2025-02-15 18:31:13 -0500

    Update tab navigation styling with new color scheme and icon sizes

--- app/page.js
+++ app/page.js
@@ -182,48 +182,48 @@ const App = () => {
                 <nav className="flex justify-between" aria-label="Tabs">
                   <button
                     className={`px-4 py-2 text-sm font-medium flex flex-row items-center gap-2 ${activeTab === "laboratorios"
-                        ? "border-b-2 border-blue-500 text-blue-600"
+                        ? " border-[#f9dbbd] font-extrabold text-3xl text-[#b0c4b1] bg-[#4a5759]/70 rounded-lg"
                         : "text-gray-500 hover:text-gray-700"
                       }`}
                     onClick={() => setActiveTab("laboratorios")}
                   >
-                    <BeakerIcon className="size-5" />
+                    <BeakerIcon className="size-6" />
                     <span>Laboratorios</span>
                   </button>
 
 
                   <button
-                    className={`px-4 py-2 text-sm font-medium flex flex-row gap-2 ${activeTab === "rx"
-                        ? "border-b-2 border-blue-500 text-blue-600"
+                    className={`px-4 py-2 text-sm font-medium flex flex-row items-center gap-2 ${activeTab === "rx"
+                        ? " border-[#f9dbbd] font-extrabold text-3xl text-[#b0c4b1] bg-[#4a5759]/70 rounded-lg"
                         : "text-gray-500 hover:text-gray-700"
                       }`}
                     onClick={() => setActiveTab("rx")}
                   >
-                    <BoltIcon className="size-5" />
+                    <BoltIcon className="size-6" />
                     <span>RX</span>
                   </button>
 
 
                   <button
-                    className={`px-4 py-2 text-sm font-medium flex flex-row gap-2 ${activeTab === "mamografias"
-                        ? "border-b-2 border-blue-500 text-blue-600"
+                    className={`px-4 py-2 text-sm font-medium flex flex-row items-center gap-2 ${activeTab === "mamografias"
+                        ? " border-[#f9dbbd] font-extrabold text-3xl text-[#b0c4b1] bg-[#4a5759]/70 rounded-lg"
                         : "text-gray-500 hover:text-gray-700"
                       }`}
                     onClick={() => setActiveTab("mamografias")}
                   >
-                    <CircleStackIcon className="size-5" />
+                    <CircleStackIcon className="size-6" />
                     <span>Mamografías</span>
                   </button>
 
 
                   <button
-                    className={`px-4 py-2 text-sm font-medium flex flex-row gap-2 ${activeTab === "ecografias"
-                        ? "border-b-2 border-blue-500 text-blue-600"
+                    className={`px-4 py-2 text-sm font-medium flex flex-row items-center gap-2 ${activeTab === "ecografias"
+                        ? " border-[#f9dbbd] font-extrabold text-3xl text-[#b0c4b1] bg-[#4a5759]/70 rounded-lg"
                         : "text-gray-500 hover:text-gray-700"
                       }`}
                     onClick={() => setActiveTab("ecografias")}
                   >
-                    <SignalIcon className="size-5" />
+                    <SignalIcon className="size-8" />
                     Ecografías
                   </button>
 
