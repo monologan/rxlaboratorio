86ef8584c0bd720daf5904523bfb69dfe8208b0a
Gerardo Valdes <monologan@gmail.com>
2025-02-26 12:54:08 -0500

    Configure Next.js for static export and disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// filepath: /c:/Users/HRCVADMSIS01/Desktop/ConsultaImagenologia/frontend/rxlaboratorio/next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Deshabilita la optimización de imágenes
+  },
+};
 
 export default nextConfig;
